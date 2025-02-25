e33e669883ef48ae7a8b583ee0b93ced6a685768
rajankumar05 <30655946+rajankumar05@users.noreply.github.com>
2025-02-24 23:07:15 -0800

    Updated DMopen API img script to render as picture scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -53,12 +53,145 @@ function buildAutoBlocks(main) {
   }
 }
 
+/*
+  * Appends query params to a URL
+  * @param {string} url The URL to append query params to
+  * @param {object} params The query params to append
+  * @returns {string} The URL with query params appended
+  * @private
+  * @example
+  * appendQueryParams('https://example.com', { foo: 'bar' });
+  * // returns 'https://example.com?foo=bar'
+*/
+function appendQueryParams(url, params) {
+  const { searchParams } = url;
+  params.forEach((value, key) => {
+    searchParams.set(key, value);
+  });
+  url.search = searchParams.toString();
+  return url.toString();
+}
+
+/**
+ * Gets the extension of a URL.
+ * @param {string} url The URL
+ * @returns {string} The extension
+ * @private
+ * @example
+ * get_url_extension('https://example.com/foo.jpg');
+ * // returns 'jpg'
+ * get_url_extension('https://example.com/foo.jpg?bar=baz');
+ * // returns 'jpg'
+ * get_url_extension('https://example.com/foo');
+ * // returns ''
+ * get_url_extension('https://example.com/foo.jpg#qux');
+ * // returns 'jpg'
+ */
+function getUrlExtension(url) {
+  return url.split(/[#?]/)[0].split('.').pop().trim();
+}
+
+function isExternalImage(element, externalImageMarker) {
+  // if the element is not an anchor, it's not an external image
+  if (element.tagName !== 'A') return false;
+
+  // if the element is an anchor with the external image marker as text content,
+  // it's an external image
+  if (element.textContent.trim() === externalImageMarker) {
+    return true;
+  }
+
+  // if the element is an anchor with the href as text content and the href has
+  // an image extension, it's an external image
+  const ext = getUrlExtension(element.getAttribute('href'));
+  return (ext && ['jpg', 'jpeg', 'png', 'gif', 'webp'].includes(ext.toLowerCase()));
+}
+
+export function createOptimizedPicture(src, alt = '', eager = false, breakpoints = [{ media: '(min-width: 600px)', width: '2000' }, { width: '750' }]) {
+  const isAbsoluteUrl = /^https?:\/\//i.test(src);
+
+  // Fallback to createOptimizedPicture if src is not an absolute URL
+  if (!isAbsoluteUrl) return libCreateOptimizedPicture(src, alt, eager, breakpoints);
+
+  const url = new URL(src);
+  const picture = document.createElement('picture');
+  const { pathname } = url;
+  const ext = pathname.substring(pathname.lastIndexOf('.') + 1);
+
+  // webp
+  breakpoints.forEach((br) => {
+    const source = document.createElement('source');
+    if (br.media) source.setAttribute('media', br.media);
+    source.setAttribute('type', 'image/webp');
+    const searchParams = new URLSearchParams({ width: br.width, format: 'webply' });
+    source.setAttribute('srcset', appendQueryParams(url, searchParams));
+    picture.appendChild(source);
+  });
+
+  // fallback
+  breakpoints.forEach((br, i) => {
+    const searchParams = new URLSearchParams({ width: br.width, format: ext });
+
+    if (i < breakpoints.length - 1) {
+      const source = document.createElement('source');
+      if (br.media) source.setAttribute('media', br.media);
+      source.setAttribute('srcset', appendQueryParams(url, searchParams));
+      picture.appendChild(source);
+    } else {
+      const img = document.createElement('img');
+      img.setAttribute('loading', eager ? 'eager' : 'lazy');
+      img.setAttribute('alt', alt);
+      picture.appendChild(img);
+      img.setAttribute('src', appendQueryParams(url, searchParams));
+    }
+  });
+
+  return picture;
+}
+
+function decorateExternalImages(ele, deliveryMarker) {
+  const extImages = ele.querySelectorAll('a');
+  extImages.forEach((extImage) => {
+    if (isExternalImage(extImage, deliveryMarker)) {
+      const extImageSrc = createOptimizedSrc(extImage.getAttribute('href'));
+      const extPicture = createOptimizedPicture(extImageSrc);
+
+      /* copy query params from link to img */
+      const extImageUrl = new URL(extImageSrc);
+      const { searchParams } = extImageUrl;
+      extPicture.querySelectorAll('source, img').forEach((child) => {
+        if (child.tagName === 'SOURCE') {
+          const srcset = child.getAttribute('srcset');
+          if (srcset) {
+            const queryParams = appendQueryParams(new URL(srcset, extImageSrc), searchParams);
+            child.setAttribute('srcset', queryParams);
+          }
+        } else if (child.tagName === 'IMG') {
+          const src = child.getAttribute('src');
+          if (src) {
+            const queryParams = appendQueryParams(new URL(src, extImageSrc), searchParams);
+            child.setAttribute('src', queryParams);
+          }
+        }
+      });
+      extImage.parentNode.replaceChild(extPicture, extImage);
+    }
+  });
+}
+
 /**
  * Decorates the main element.
  * @param {Element} main The main element
  */
 // eslint-disable-next-line import/prefer-default-export
 export function decorateMain(main) {
+
+  // decorate external images with explicit external image marker
+  decorateExternalImages(main, '//External Image//');
+
+  // decorate external images with implicit external image marker
+  decorateExternalImages(main);
+  
   // hopefully forward compatible button decoration
   decorateButtons(main);
   decorateIcons(main);
