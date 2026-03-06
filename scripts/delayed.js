ad86f3dc15901471b9b01cd87e5ea6dfb7e6ebec
rajankumar05 <30655946+rajankumar05@users.noreply.github.com>
2026-03-06 15:36:19 -0800

    Update delayed.js

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -4,7 +4,8 @@ import { loadScript } from './aem.js';
   await loadScript('https://assets.adobedtm.com/7c9efc5b1aec/67289d24beda/launch-139ab5a4b298.min.js');
 export async function addCookieBanner() {
   const cookieBanner = document.createElement('div');
-  cookieBanner.classList.add('consent-banner');
+  cookieBanner.id = 'consent-banner';
+  //cookieBanner.classList.add('consent-banner');
   cookieBanner.innerHTML = `<div id="teconsent"></div>
 </div>`;
   document.querySelector('main').append(cookieBanner);
