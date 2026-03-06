042b9227cfa4d4eb4a2ec669ae539ba9389bd132
rajankumar05 <30655946+rajankumar05@users.noreply.github.com>
2026-03-06 13:10:44 -0800

    Update delayed.js

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,11 +1,5 @@
 // add delayed functionality here
 import { loadScript } from './aem.js';
 
-function AdobeLaunch() {
-  this.loadGTM = () => {
   await loadScript('https://assets.adobedtm.com/7c9efc5b1aec/67289d24beda/launch-139ab5a4b298.min.js');
-  };
-}
-if (!window.location.hostname.includes('localhost') && !document.location.hostname.includes('.hlx.page')) {
-  new AdobeLauch().loadGTM();
 }
