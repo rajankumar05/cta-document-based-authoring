4821480508012c28fe53ec9606710e39e53cc4a9
rajankumar05 <30655946+rajankumar05@users.noreply.github.com>
2026-03-09 12:10:27 -0700

    Update delayed.js

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,7 +1,7 @@
 // add delayed functionality here
 import { loadScript } from './aem.js';
 
-  await loadScript('https://assets.adobedtm.com/7c9efc5b1aec/67289d24beda/launch-139ab5a4b298.min.js');
+loadScript('https://consent.trustarc.com/v2/notice/jwqzim');
 export async function addCookieBanner() {
   const cookieBanner = document.createElement('div');
   cookieBanner.id = 'consent-banner';
