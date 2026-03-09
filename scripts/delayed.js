b0b20836f36bad08bce06b72a5ee74faa94fb9f5
rajankumar05 <30655946+rajankumar05@users.noreply.github.com>
2026-03-09 12:13:19 -0700

    Update delayed.js

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -5,7 +5,6 @@ loadScript('https://consent.trustarc.com/v2/notice/jwqzim');
 export async function addCookieBanner() {
   const cookieBanner = document.createElement('div');
   cookieBanner.id = 'consent-banner';
-  //cookieBanner.classList.add('consent-banner');
   cookieBanner.innerHTML = `<div id="teconsent"></div>
 </div>`;
   document.querySelector('main').append(cookieBanner);
